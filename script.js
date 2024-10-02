910f9a0fee5c9eb55ec4f567ee25d5bf798f7f58
Morten Bonderup <moab@eaaa.dk>
2024-10-02 16:53:27 +0200

    Private attribute

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 class TodoList {
+    #tasks;
+
     constructor(todoList) {
-        this.tasks = todoList;
+        this.#tasks = todoList;
     }
 
     addTask() {
         const taskInput = document.getElementById('new-task');
         const taskDescription = taskInput.value.trim();
         if (taskDescription !== "") {
-            this.tasks.push(taskDescription);
+            this.#tasks.push(taskDescription);
             taskInput.value = '';
             this.render();
         }
     }
 
     deleteTask(index) {
-        this.tasks.splice(index, 1);
+        this.#tasks.splice(index, 1);
         this.render();
     }
 
     render() {
         const taskList = document.getElementById('task-list');
         taskList.innerHTML = '';
-        for (let i = 0; i < this.tasks.length; i++) {
+        for (let i = 0; i < this.#tasks.length; i++) {
             const taskItem = document.createElement('li');
             taskItem.innerHTML = `
-                <span>${this.tasks[i]}</span>
+                <span>${this.#tasks[i]}</span>
                 <button class="delete" onclick="myTodoList.deleteTask(${i})">Delete</button>
             `;
             taskList.appendChild(taskItem);
